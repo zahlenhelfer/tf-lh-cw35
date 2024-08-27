f85b7b150cbc5978cf83098634f0b8c78b811145
Marcus Ross <marcus.ross@zahlenhelfer.de>
2024-08-27 20:14:33 +0200

    added a new vpc

--- vpc.tf
+++ vpc.tf
@@ -1,25 +1,44 @@
 resource "aws_vpc" "my_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
+
   tags = {
-    Name = "Mein toller VPC"
+    Name = "My VPC"
   }
 }
 
-resource "aws_subnet" "public_1a" {
+resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "eu-central-1a"
+
   tags = {
-    Name = "Public Subnet-1a"
+    Name = "Public Subnet"
   }
 }
 
-resource "aws_subnet" "public_1b" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-central-1b"
+resource "aws_internet_gateway" "my_vpc_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
   tags = {
-    Name = "Public Subnet-1b"
+    Name = "My VPC - Internet Gateway"
   }
+}
+
+resource "aws_route_table" "my_vpc_eu_central_1a_public" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_vpc_igw.id
+  }
+
+  tags = {
+    Name = "Public Subnet Route Table."
+  }
+}
+
+resource "aws_route_table_association" "my_vpc_eu_central_1a_public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.my_vpc_eu_central_1a_public.id
 }
