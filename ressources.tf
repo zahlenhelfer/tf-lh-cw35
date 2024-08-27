414976ad017dc2939053f01857af5e4cbc8ac4ce
Marcus Ross <marcus.ross@zahlenhelfer.de>
2024-08-27 17:49:52 +0200

    feat: Add ingress rule for port 80 in aws_security_group
    The code changes in ressources.tf add an ingress rule for port 80 in the aws_security_group resource. This allows incoming traffic on port 80 to the associated EC2 instances. This update improves the accessibility of the web server.
    
    Recent user commits:
    - feat: Update AMI validation in variables.tf
    - feat: Update AWS instance resource in main.tf
    - refactor the file-layout
    
    Recent repository commits:
    - chore: Update .gitignore to include .terraform.lock.hcl file
    - chore: Add .gitignore and main.tf files

--- ressources.tf
+++ ressources.tf
@@ -26,4 +26,11 @@ resource "aws_security_group" "webserver_access" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
