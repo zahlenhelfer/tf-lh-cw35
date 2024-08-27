8eee951f99764a75baa281af1aa415cd2aa5c90f
Marcus Ross <marcus.ross@zahlenhelfer.de>
2024-08-27 19:56:44 +0200

    feat: Add AWS subnets for public availability zones
    The code changes in vpc.tf add AWS subnets for the public availability zones "eu-central-1a" and "eu-central-1b". These subnets have CIDR blocks "10.0.0.0/24" and "10.0.1.0/24" respectively. Each subnet is tagged with a descriptive name. This update improves the network configuration by creating separate subnets for different availability zones.

--- vpc.tf
+++ vpc.tf
@@ -5,3 +5,21 @@ resource "aws_vpc" "my_vpc" {
     Name = "Mein toller VPC"
   }
 }
+
+resource "aws_subnet" "public_1a" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "eu-central-1a"
+  tags = {
+    Name = "Public Subnet-1a"
+  }
+}
+
+resource "aws_subnet" "public_1b" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-central-1b"
+  tags = {
+    Name = "Public Subnet-1b"
+  }
+}
