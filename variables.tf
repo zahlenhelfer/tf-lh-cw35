1cfa924a298c9a7a3818a0481b11aaa6a9fd1ee9
Marcus Ross <marcus.ross@zahlenhelfer.de>
2024-08-27 16:43:30 +0200

    feat: Update AMI validation in variables.tf
    The code changes in variables.tf include adding a validation block for the "ami" variable. The validation condition checks if the length of the AMI value is greater than 4 and if it starts with "ami-". If the condition is not met, an error message is displayed. This update improves the validation of the AMI value.
    
    Recent user commits:
    - feat: Update AWS instance resource in main.tf
    - refactor the file-layout
    
    Recent repository commits:
    - chore: Update .gitignore to include .terraform.lock.hcl file
    - chore: Add .gitignore and main.tf files

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "ami" {
   type        = string
   description = "value of the AMI"
   default     = "ami-0de02246788e4a354"
+  validation {
+    condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+    error_message = "Image-id value must be a valid ami id, does it start with 'ami-'?"
+  }
 }
 
 variable "instance_type" {
