c8ce1c89597ef932911f3546377141bf60e3d985
Marcus Ross <marcus.ross@zahlenhelfer.de>
2024-08-27 15:41:49 +0200

    feat: Update AWS instance resource in main.tf
    The code changes modify the count of the AWS instance resource in the main.tf file. The count is changed from 2 to 1. This change reduces the number of instances created. The tags for identifying the instance are also updated to include the count index.
    
    Recent user commits:
    - feat: Add AWS instance resource to main.tf
    - refactor the file-layout
    
    Recent repository commits:
    - chore: Update .gitignore to include .terraform.lock.hcl file
    - chore: Add .gitignore and main.tf files

--- ressources.tf
+++ ressources.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "app_server" {
-  count         = 2
+  count         = 1
   ami           = var.ami
   instance_type = var.instance_type
   tags = {
-    Name         = "ExampleAppServerInstance"
+    Name         = "App Server ${count.index + 1}"
     KRITIS       = "false"
     Kostenstelle = "4711"
   }
